09818dfe596b1ae8d59149466fd04edf4b8e9c65
Markus <markus.marlinus.z@mail.ukrim.ac.id>
2026-06-08 23:18:59 +0700

    CRUD Pengelola Akun Staff

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,12 @@
+<h1>Data User Staff</h1>
+
+<table border="1">
+    <tr>
+        <th>Nama</th>
+        <th>Email</th>
+    </tr>
+    <tr>
+        <td>Admin</td>
+        <td>admin@example.com</td>
+    </tr>
+</table>
